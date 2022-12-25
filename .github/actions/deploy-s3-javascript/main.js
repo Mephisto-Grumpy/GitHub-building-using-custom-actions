6a88fb4891cbedf3bf3ca46b2d8c74e8786013f3
PunGrumpy <noppakorn.pun@gmail.com>
2022-12-25 20:52:11 +0700

    try change aws to do

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -9,10 +9,18 @@ function run() {
   const distFolder = core.getInput('dist-folder', { required: true })
 
   // 2) Upload files
-  const s3Uri = `s3://${bucket}`
-  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+  // AWS S3
+  // const s3Uri = `s3://${bucket}`
+  // exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
 
-  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion.amazonaws.com}`
+  // DigitalOcean
+  exec.exec(`doctl spaces sync ${distFolder} ${bucket}`)
+
+  // AWS
+  // const websiteUrl = `http://${bucket}.s3-website-${bucketRegion.amazonaws.com}`
+
+  // DigitalOcean
+  const websiteUrl = `https://${bucket}.nyc3.digitaloceanspaces.com`
   core.setOutput('website-url', websiteUrl) // ::set-output
 }
 
